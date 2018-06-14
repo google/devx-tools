fc5c0a4e133885345e1ff439e2d4cc9902818761
Mauricio Galindo <mauriciogg@google.com>
2018-06-14 13:15:16 -0700

    Adds push/pull implementation for waterfall server
    Change-Id: I68c43547e123a962b591e1efc07bf6bade98733d

--- waterfall/server/server.go
+++ waterfall/server/server.go
@@ -4,9 +4,14 @@ package server
 import (
 	"context"
 	"io"
+	"os"
 
 	waterfall_grpc "github.com/waterfall/proto/waterfall_go_grpc"
+	"github.com/waterfall/stream"
+	"golang.org/x/sync/errgroup"
 )
+
+const bufSize = 32 * 1024
 
 // WaterfallServer implements the waterfall gRPC service
 type WaterfallServer struct {
@@ -29,6 +34,91 @@ func (s *WaterfallServer) Echo(stream waterfall_grpc.Waterfall_EchoServer) error
 			return err
 		}
 	}
+}
+
+// Push untars a file/dir that it receives over a gRPC stream
+func (s *WaterfallServer) Push(rpc waterfall_grpc.Waterfall_PushServer) error {
+	xfer, err := rpc.Recv()
+	if err != nil {
+		return err
+	}
+
+	r, w := io.Pipe()
+	defer r.Close()
+	defer w.Close()
+
+	// Connect the gRPC stream with a reader that untars
+	// the contents of the stream into the desired path
+	eg, _ := errgroup.WithContext(s.ctx)
+	eg.Go(func() error {
+		return stream.Untar(r, xfer.Path)
+	})
+
+	eg.Go(func() error {
+		for {
+			if err == io.EOF {
+				return rpc.SendAndClose(
+					&waterfall_grpc.Transfer{Success: true})
+			}
+			if err != nil {
+				return err
+			}
+			if _, err := w.Write(xfer.Payload); err != nil {
+				return rpc.SendAndClose(
+					&waterfall_grpc.Transfer{
+						Success: false,
+						Err:     []byte(err.Error())})
+			}
+			xfer, err = rpc.Recv()
+		}
+	})
+	return eg.Wait()
+}
+
+// Pull tars up a file/dir and sends it over a gRPC stream
+func (s *WaterfallServer) Pull(t *waterfall_grpc.Transfer, ps waterfall_grpc.Waterfall_PullServer) error {
+	if _, err := os.Stat(t.Path); os.IsNotExist(err) {
+		return err
+	}
+
+	r, w := io.Pipe()
+	defer r.Close()
+	defer w.Close()
+
+	// Connect a writer that traverses a the filesystem
+	// tars the contents to the gRPC stream
+	eg, _ := errgroup.WithContext(s.ctx)
+	eg.Go(func() error {
+		defer w.Close()
+		return stream.Tar(w, t.Path)
+	})
+
+	buff := make([]byte, bufSize)
+	eg.Go(func() error {
+		defer r.Close()
+
+		for {
+			n, err := r.Read(buff)
+			if err != nil && err != io.EOF {
+				return err
+			}
+
+			if n > 0 {
+				// No need to populate anything else. Sending the path
+				// everytime is just overhead.
+				xfer := &waterfall_grpc.Transfer{Payload: buff[0:n]}
+				if err := ps.Send(xfer); err != nil {
+					return err
+				}
+			}
+
+			if err == io.EOF {
+				return nil
+			}
+		}
+	})
+	err := eg.Wait()
+	return err
 }
 
 // NewWaterfallServer returns a gRPC server that implements the waterfall service
