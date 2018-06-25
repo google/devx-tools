ad37b4b512176dd28b7bd0b0572b051c6bdb74ac
Mauricio Galindo <mauriciogg@google.com>
2018-06-25 16:59:59 -0700

    added grpc kv store for recording device names to device endpoings
    Change-Id: I421441df81a5e2088174d2041a26515101ba3de9

---
+++ waterfall/registry/registry.go
@@ -0,0 +1,57 @@
+// Package registry implements the Registry service as very simple/naive KV store
+package registry
+
+import (
+	"context"
+	"fmt"
+
+	waterfall_grpc "github.com/waterfall/proto/waterfall_go_grpc"
+)
+
+type registryServer struct {
+	r map[string]string
+}
+
+// Add adds a new device to the registry of devices
+func (s *registryServer) Add(ctx context.Context, e *waterfall_grpc.Entry) (*waterfall_grpc.OpResult, error) {
+	if e.Key == "" {
+		return nil, fmt.Errorf("entry key was not specified.")
+	}
+
+	if e.Val == "" {
+		return nil, fmt.Errorf("entry value was not specified.")
+	}
+
+	s.r[e.Key] = e.Val
+	return &waterfall_grpc.OpResult{Status: waterfall_grpc.OpResult_SUCCESS}, nil
+}
+
+func (s *registryServer) Remove(ctx context.Context, e *waterfall_grpc.Entry) (*waterfall_grpc.OpResult, error) {
+	// Removing the empty string from the map will succeed, however this
+	// is most likely an error on the client side.
+	if e.Key == "" {
+		return nil, fmt.Errorf("entry key was not specified.")
+	}
+	delete(s.r, e.Key)
+	return &waterfall_grpc.OpResult{Status: waterfall_grpc.OpResult_SUCCESS}, nil
+}
+
+func (s *registryServer) Get(ctx context.Context, e *waterfall_grpc.Entry) (*waterfall_grpc.Entry, error) {
+	val, ok := s.r[e.Key]
+	if !ok {
+		return &waterfall_grpc.OpResult{Status: waterfall_grpc.OpResult_KEY_NOT_FOUND}, nil
+	}
+	return &waterfall_grpc.OpResult{
+		Status: waterfall_grpc.OpResult_SUCCESS,
+		Entry: &waterfall_grpc.Entry{Key: e.Key, Val: val}}, nil
+}
+
+// NewRegistryServer creates a new empty registryServer
+func NewRegistryServer(ctx context.Context) *registryServer {
+	return NewRegistryServerWithEntries(make(map[string]string))
+}
+
+// NewRegistryServerWithEntries creates a new registryServer populated with entries
+func NewRegistryServerWithEntries(entries map[string]string) *registryServer {
+	return &registryServer{r: entries}
+}
---
+++ waterfall/registry/registry_bin.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net"
+	"strings"
+
+	waterfall_grpc "github.com/waterfall/proto/waterfall_go_grpc"
+	"github.com/waterfall/registry"
+	"google.golang.org/grpc"
+)
+
+var (
+	addr     = flag.String("addr", "localhost:13331", "network address in which to listen")
+	unixPipe = flag.String(
+		"unix_pipe", "h2o_waterfall_registry", "Name of abstract unix pipe to listen on")
+	mode    = flag.String("mode", "unix", "Type of connection <unix|TCP>")
+	entries = flag.String("entries", "", "List of comma separated k=v entries")
+)
+
+func main() {
+	flag.Parse()
+
+	log.Println("Starting kv store in port ...\n")
+
+	var lis net.Listener
+	var err error
+	switch *mode {
+	case "unix":
+		lis, err = net.Listen("unix", "@"+*unixPipe)
+	case "tcp":
+		lis, err = net.Listen("tcp", *addr)
+	default:
+		log.Fatalf("Unuspported mode %s", *mode)
+	}
+
+	if err != nil {
+		log.Fatalf("failed to listen %v", err)
+	}
+	defer lis.Close()
+
+	es := make(map[string]string)
+	if *entries != "" {
+		for _, e := range strings.Split(*entries, ",") {
+			pts := strings.Split(e, "=")
+			if len(pts) != 2 {
+				log.Fatalf("Malformed entry %s", e)
+			}
+			es[pts[0]] = pts[1]
+		}
+	}
+
+	grpcServer := grpc.NewServer()
+	waterfall_grpc.RegisterRegistryServer(grpcServer, registry.NewRegistryServerWithEntries(es))
+
+	log.Println("Serving ...")
+	grpcServer.Serve(lis)
+}
