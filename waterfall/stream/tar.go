0706ad97136dbed836137e4d6a60efbe4b4d34c6
Mauricio Galindo <mauriciogg@google.com>
2018-07-23 11:34:19 -0700

    handle symlinks, singlefiles and multiple push pull edge cases
    Change-Id: I3df4bed72e9a291e01fd085ce412167a6c6ab5df

--- waterfall/stream/tar.go
+++ waterfall/stream/tar.go
@@ -3,36 +3,85 @@ package stream
 
 import (
 	"archive/tar"
-	"errors"
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 var modeDir os.FileMode = 0755
 
-// Untar untars the contents of a reader into path
-func Untar(r io.Reader, path string) error {
+// Untar untars the contents of a reader into dst
+func Untar(r io.Reader, dst string) error {
 	tr := tar.NewReader(r)
-	for {
-		hr, err := tr.Next()
+	root := dst
 
-		if err == io.EOF {
-			return nil
-		}
-		if err != nil {
-			return err
-		}
+	// Special case the first file to figure out where to unpack stream.
+	hdr, err := tr.Next()
+	if err != nil {
+		return err
+	}
 
-		target := filepath.Join(path, hr.Name)
-		switch hr.Typeflag {
-		case tar.TypeReg:
-			if err := os.MkdirAll(filepath.Dir(target), modeDir); err != nil {
-				return err
+	var name string
+	var rootReplace string
+
+	// ferr is used to determine where to unpack the stream
+	fi, ferr := os.Stat(dst)
+	switch hdr.Typeflag {
+	case tar.TypeSymlink:
+		fallthrough
+	case tar.TypeRegA:
+		fallthrough
+	case tar.TypeReg:
+		if ferr == nil {
+			// dst exists and user specified directory as dst
+			// eg push /foo/bar/file.txt /baz/some_dir
+			if fi.Mode().IsDir() {
+				name = filepath.Join(dst, hdr.Name)
+				root = dst
+			} else {
+				// dst exists and user specified file name as dst
+				// eg push /foo/bar/file.txt /baz/some_dir/file.txt
+				name = dst
+				root = filepath.Dir(dst)
 			}
-			f, err := os.OpenFile(
-				target, os.O_CREATE|os.O_RDWR, os.FileMode(hr.Mode))
+			break
+		}
+		if os.IsNotExist(ferr) {
+			// dst does not exist push to parent dir and rename to dst.
+			root = filepath.Dir(dst)
+			if ferr = os.MkdirAll(root, modeDir); ferr == nil {
+				name = dst
+				break
+			}
+		}
+		return ferr
+	case tar.TypeDir:
+		if ferr == nil {
+			if fi.Mode().IsDir() {
+				// dst exists and is a directory. Create dir in directory
+				name = filepath.Join(dst, hdr.Name)
+				break
+			}
+			return fmt.Errorf("file %s exists and is not a directory", dst)
+		} else if os.IsNotExist(ferr) {
+			root = dst
+			name = dst
+			rootReplace = hdr.Name
+			break
+		}
+		return ferr
+	}
+
+	for {
+		switch hdr.Typeflag {
+		case tar.TypeRegA:
+			fallthrough
+		case tar.TypeReg:
+			os.RemoveAll(name)
+			f, err := os.OpenFile(name, os.O_CREATE|os.O_RDWR, os.FileMode(hdr.Mode))
 			if err != nil {
 				return err
 			}
@@ -45,18 +94,23 @@ func Untar(r io.Reader, path string) error {
 				return err
 			}
 		case tar.TypeDir:
-			if err := os.MkdirAll(target, modeDir); err != nil {
+			if err := os.MkdirAll(name, modeDir); err != nil {
 				return err
 			}
-		case tar.TypeLink:
-			return errors.New("error: unimplemented mode TypeLink")
 		case tar.TypeSymlink:
-			return errors.New("error: unimplemented mode TypeSymLink")
-		case tar.TypeXGlobalHeader:
-			return errors.New("error: unimplemented mode TypeSymLink")
-		default:
-			return errors.New("error: unimplemented mode XGlobalHeader")
+			if err := os.Symlink(hdr.Linkname, name); err != nil {
+				return err
+			}
 		}
+
+		hdr, err = tr.Next()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		name = filepath.Join(root, strings.TrimPrefix(hdr.Name, rootReplace))
 	}
 }
 
@@ -65,33 +119,74 @@ func Tar(writer io.Writer, src string) error {
 	if _, err := os.Stat(src); err != nil {
 		return fmt.Errorf("Unable to tar files - %v", err.Error())
 	}
-	baseDir := src
+
+	// avoid making the directory relative to itself
+	src = strings.TrimRight(src, "/")
 
 	tw := tar.NewWriter(writer)
 	defer tw.Close()
 
 	return filepath.Walk(src, func(file string, fi os.FileInfo, err error) error {
-
 		if err != nil {
 			return err
 		}
 
-		header, err := tar.FileInfoHeader(fi, fi.Name())
-		if err != nil {
+		var hdr *tar.Header
+		if fi.Mode()&os.ModeSymlink != 0 {
+			t, err := filepath.EvalSymlinks(file)
+			if err != nil {
+				// Don't include broken symlinks
+				log.Printf("Skipping broken link %s -> %s\n", file)
+				return nil
+			}
+
+			// ok to ignore error file always lives inside src
+			sr, _ := filepath.Rel(src, file)
+			name := filepath.Join(filepath.Base(src), sr)
+			if rel, err := filepath.Rel(src, t); err == nil {
+				// Pointee lives under same root as pointer, so write the symlink.
+				if hdr, err = tar.FileInfoHeader(fi, rel); err != nil {
+					return err
+				}
+				hdr.Name = name
+			} else {
+				// Pointee lives outside pointer root, write the file pointed to.
+				log.Printf("Resolving symlink %s before pushing\n", file)
+				var ti os.FileInfo
+				if ti, err = os.Stat(t); err != nil {
+					// Skip broken links. Error out on everything else.
+					if os.IsNotExist(err) {
+						log.Printf("Skipping broken link %s -> %s\n", file, t)
+						return nil
+					}
+					return err
+				}
+				if !ti.Mode().IsRegular() {
+					return nil
+				}
+				if hdr, err = tar.FileInfoHeader(ti, ""); err != nil {
+					return err
+				}
+				hdr.Name = name
+			}
+		} else {
+			hdr, err = tar.FileInfoHeader(fi, fi.Name())
+			if err != nil {
+				return err
+			}
+
+			rel, err := filepath.Rel(filepath.Dir(src), file)
+			if err != nil {
+				return err
+			}
+			hdr.Name = rel
+		}
+
+		if err := tw.WriteHeader(hdr); err != nil {
 			return err
 		}
 
-		rel, err := filepath.Rel(filepath.Dir(baseDir), file)
-		if err != nil {
-			return err
-		}
-		header.Name = rel
-
-		if err := tw.WriteHeader(header); err != nil {
-			return err
-		}
-
-		if !fi.Mode().IsRegular() {
+		if (hdr.Typeflag != tar.TypeReg && hdr.Typeflag != tar.TypeRegA) || hdr.Size == 0 {
 			return nil
 		}
 
--- waterfall/stream/tar_test.go
+++ waterfall/stream/tar_test.go
@@ -8,41 +8,54 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 	"testing"
+)
+
+const (
+	kindFile = iota
+	kindSymlink
 )
 
 var (
 	contents = testBytes()
 
+	rootVar = "$ROOT"
+
+	singleFile = fs{path: "", files: []file{file{name: "foo.txt"}}}
+	dir        = fs{path: "foo", files: []file{file{name: "a.txt"}, file{name: "b.txt"}}}
+
 	cases = []fs{
+		singleFile,
+		dir,
 		fs{
-			path:  "",
-			files: []string{"foo.txt"},
-		},
-		fs{
-			path:  "foo",
-			files: []string{"a.txt", "b.txt"},
-		},
-		fs{
-			path:  "zoo",
-			files: []string{"a.txt", "b.txt"},
+			path: "zoo",
+			files: []file{
+				file{name: "a_symlink", kind: kindSymlink, link: rootVar + "/zoo/baz/d.txt"},
+				file{name: "a.txt"},
+				file{name: "b.txt"}},
 			dirs: []fs{
+				fs{path: "zoo/bar"},
 				fs{
-					path: "zoo/bar",
-				},
-				fs{
-					path:  "zoo/baz",
-					files: []string{"d.txt"},
+					path: "zoo/baz",
+					files: []file{
+						file{name: "d.txt"}},
 					dirs: []fs{
 						fs{
 							path:  "zoo/baz/qux",
-							files: []string{"e.txt"},
+							files: []file{file{name: "e.txt"}},
 						}}}}}}
 )
 
+type file struct {
+	name string
+	link string
+	kind int
+}
+
 type fs struct {
 	path  string
-	files []string
+	files []file
 	dirs  []fs
 }
 
@@ -56,10 +69,22 @@ func makeFs(baseDir string, tree fs) ([]string, error) {
 		}
 	}
 
+	bits := testBytes()
 	for _, f := range tree.files {
-		seen = append(seen, filepath.Join(tree.path, f))
-		if err := makeTestFile(filepath.Join(dirPath, f), testBytes()); err != nil {
-			return nil, err
+		seen = append(seen, filepath.Join(tree.path, f.name))
+		switch f.kind {
+		case kindFile:
+			if err := ioutil.WriteFile(filepath.Join(dirPath, f.name), bits, 0655); err != nil {
+				return nil, err
+			}
+		case kindSymlink:
+			link := f.link
+			if strings.Contains(link, rootVar) {
+				link = filepath.Join(baseDir, strings.Split(link, rootVar+"/")[1])
+			}
+			if err := os.Symlink(link, filepath.Join(dirPath, f.name)); err != nil {
+				return nil, err
+			}
 		}
 	}
 
@@ -75,7 +100,7 @@ func makeFs(baseDir string, tree fs) ([]string, error) {
 
 func testBytes() []byte {
 	// Just create a sequential byte stream to avoid having to create test files
-	b := make([]byte, 64*1024)
+	b := make([]byte, 1024)
 	bb := bytes.NewBuffer(b)
 	var i uint32
 	for i = 0; i < uint32(len(b)); i++ {
@@ -84,8 +109,15 @@ func testBytes() []byte {
 	return bb.Bytes()
 }
 
-func makeTestFile(path string, bits []byte) error {
-	return ioutil.WriteFile(path, bits, 0655)
+func fileCompare(f string) error {
+	bs, err := ioutil.ReadFile(f)
+	if err != nil {
+		return err
+	}
+	if bytes.Compare(contents, bs) != 0 {
+		return fmt.Errorf("bytes for file %s not as expected", f)
+	}
+	return nil
 }
 
 func dirCompare(dir1, dir2, path string, info os.FileInfo, seen map[string]bool) error {
@@ -102,14 +134,7 @@ func dirCompare(dir1, dir2, path string, info os.FileInfo, seen map[string]bool)
 	if _, err := os.Stat(rf); os.IsNotExist(err) {
 		return err
 	}
-	bs, err := ioutil.ReadFile(rf)
-	if err != nil {
-		return err
-	}
-	if bytes.Compare(contents, bs) != 0 {
-		return fmt.Errorf("bytes for file %s not as expected", rel)
-	}
-	return nil
+	return fileCompare(rf)
 }
 
 func TestTar(t *testing.T) {
@@ -128,7 +153,7 @@ func TestTar(t *testing.T) {
 		tp := tc.path
 		if tc.path == "" {
 			// This is a single file test case
-			tp = tc.files[0]
+			tp = tc.files[0].name
 		}
 
 		tf, err := ioutil.TempFile("", "src.tar")
@@ -139,6 +164,7 @@ func TestTar(t *testing.T) {
 
 		if err := Tar(tf, filepath.Join(td, tp)); err != nil {
 			t.Errorf("error taring file: %v", err)
+			return
 		}
 
 		// Untar using reference implementation
@@ -151,7 +177,7 @@ func TestTar(t *testing.T) {
 		args := []string{"-C", tr, "-xf", tf.Name()}
 		cmd := exec.Command("tar", args...)
 		if out, err := cmd.CombinedOutput(); err != nil {
-			t.Errorf("error untaring cmd %v: %s, %v", args, out, err)
+			t.Fatalf("error untaring cmd %v: %s, %v", args, out, err)
 		}
 
 		// Walk the original dir and compare against extracted
@@ -165,17 +191,326 @@ func TestTar(t *testing.T) {
 
 		if err != nil {
 			t.Errorf("got error taring: %v", err)
+			return
 		}
 		if len(seen) != len(all) {
 			t.Errorf("wrong number of files. Got %v expected %v", seen, all)
+			return
 		}
 		for _, f := range all {
 			if !seen[f] {
 				t.Errorf("file %s not in tared files", f)
+				return
 			}
 		}
 	}
+}
 
+func makeTar(dir, name string) (*os.File, error) {
+	// uses tar bin to tar using reference implementation
+	tf, err := ioutil.TempFile("", "src.tar")
+	if err != nil {
+		return nil, err
+	}
+
+	// tar using reference implementation
+	args := []string{"-C", dir, "-cf", tf.Name(), name}
+	cmd := exec.Command("tar", args...)
+	if _, err := cmd.CombinedOutput(); err != nil {
+		os.RemoveAll(tf.Name())
+		return nil, err
+	}
+	return tf, nil
+}
+
+// TestUntar[A-Za-z]+ tests the different possible scenarios for the creation of
+// the first entry in the tar stream
+
+// TestUntarDirDstIsDir push /foo/bar /remote/existing_dir (dir is created inside dir)
+func TestUntarDirDstIsDir(t *testing.T) {
+	td, err := ioutil.TempDir("", "untartest")
+	if err != nil {
+		t.Fatalf("error creating temp dir: %v", err)
+	}
+	defer os.RemoveAll(td)
+
+	all, err := makeFs(td, dir)
+	if err != nil {
+		t.Fatalf("error creating temp fs: %v", err)
+	}
+
+	tp := dir.path
+	tf, err := makeTar(td, tp)
+	if err != nil {
+		t.Fatalf("error creating taring src: %v", err)
+	}
+	defer tf.Close()
+	defer os.RemoveAll(tf.Name())
+
+	tr, err := ioutil.TempDir("", "tarres")
+	if err != nil {
+		t.Fatalf("error creating temp dir: %v", err)
+	}
+	defer os.RemoveAll(tr)
+
+	if err := Untar(tf, tr); err != nil {
+		t.Errorf("error untaring file: %v", err)
+		return
+	}
+
+	// Walk the original dir and compare against extracted
+	seen := make(map[string]bool)
+	err = filepath.Walk(td, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		return dirCompare(td, tr, path, info, seen)
+	})
+
+	if err != nil {
+		t.Errorf("got error untaring: %v", err)
+		return
+	}
+	if len(seen) != len(all) {
+		t.Errorf("wrong number of files. Got %v expected %v", seen, all)
+		return
+	}
+	for _, f := range all {
+		if !seen[f] {
+			t.Errorf("file %s not in tared files", f)
+			return
+		}
+	}
+}
+
+// TestUntarDirDstIsFile push /foo/bar /remote/existing_file (invalid operation)
+func TestUntarDirDstIsFile(t *testing.T) {
+	td, err := ioutil.TempDir("", "untartest")
+	if err != nil {
+		t.Fatalf("error creating temp dir: %v", err)
+	}
+	defer os.RemoveAll(td)
+
+	if _, err := makeFs(td, dir); err != nil {
+		t.Fatalf("error creating temp fs: %v", err)
+	}
+
+	tp := dir.path
+	tf, err := makeTar(td, tp)
+	if err != nil {
+		t.Fatalf("error creating taring src: %v", err)
+	}
+	defer tf.Close()
+	defer os.RemoveAll(tf.Name())
+
+	tr, err := ioutil.TempDir("", "tarres")
+	if err != nil {
+		t.Fatalf("error creating temp dir: %v", err)
+	}
+	defer os.RemoveAll(tr)
+
+	dst := filepath.Join(tr, "file.txt")
+	f, err := os.Create(dst)
+	if err != nil {
+		t.Fatal(f)
+	}
+	f.Close()
+
+	if err := Untar(tf, dst); err == nil {
+		t.Errorf("expected error but got none")
+		return
+	}
+}
+
+// TestUntarDirDstNotExist push /foo/bar /remote/non_existing_path (dir is created in remote path)
+func TestUntarDirDstNotExist(t *testing.T) {
+	td, err := ioutil.TempDir("", "untartest")
+	if err != nil {
+		t.Fatalf("error creating temp dir: %v", err)
+	}
+	defer os.RemoveAll(td)
+
+	all, err := makeFs(td, dir)
+	if err != nil {
+		t.Fatalf("error creating temp fs: %v", err)
+	}
+
+	tp := dir.path
+	tf, err := makeTar(td, tp)
+	if err != nil {
+		t.Fatalf("error creating taring src: %v", err)
+	}
+	defer tf.Close()
+	defer os.RemoveAll(tf.Name())
+
+	tr, err := ioutil.TempDir("", "tarres")
+	if err != nil {
+		t.Fatalf("error creating temp dir: %v", err)
+	}
+	defer os.RemoveAll(tr)
+
+	newName := "new_dir"
+	dst := filepath.Join(tr, newName)
+	if err := Untar(tf, dst); err != nil {
+		t.Errorf("got error untaring: %v", err)
+		return
+	}
+
+	if err := os.Rename(filepath.Join(td, tp), filepath.Join(td, newName)); err != nil {
+		t.Fatalf("error renaming: %v", err)
+	}
+
+	// Walk the original dir and compare against extracted
+	seen := make(map[string]bool)
+	err = filepath.Walk(td, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		return dirCompare(td, tr, path, info, seen)
+	})
+
+	if err != nil {
+		t.Errorf("got error untaring: %v", err)
+		return
+	}
+	if len(seen) != len(all) {
+		t.Errorf("wrong number of files. Got %v expected %v", seen, all)
+		return
+	}
+	for _, f := range all {
+		if !seen[fmt.Sprintf("%s%s", newName, strings.TrimLeft(f, tp))] {
+			t.Errorf("file %s not in tared files", f)
+			return
+		}
+	}
+
+}
+
+// TestUntarFileDstIsDir push /foo/bar/file.txt /remote/existing_dir (file is created in dir)
+func TestUntarFileDstIsDir(t *testing.T) {
+	td, err := ioutil.TempDir("", "untartest")
+	if err != nil {
+		t.Fatalf("error creating temp dir: %v", err)
+	}
+	defer os.RemoveAll(td)
+
+	if _, err := makeFs(td, singleFile); err != nil {
+		t.Fatalf("error creating temp fs: %v", err)
+	}
+	tp := singleFile.files[0].name
+
+	tf, err := makeTar(td, tp)
+	if err != nil {
+		t.Fatalf("error creating taring src: %v", err)
+	}
+	defer tf.Close()
+	defer os.RemoveAll(tf.Name())
+
+	tr, err := ioutil.TempDir("", "tarres")
+	if err != nil {
+		t.Fatalf("error creating temp dir: %v", err)
+	}
+	defer os.RemoveAll(tr)
+
+	if err := Untar(tf, tr); err != nil {
+		t.Errorf("error untaring file: %v", err)
+		return
+	}
+
+	if _, err := os.Stat(filepath.Join(tr, tp)); err != nil {
+		t.Errorf("error untaring file: %v", err)
+	}
+}
+
+// TestUntarFileDstIsFile push /foo/bar/file.txt /remote/file.txt (replace file)
+func TestUntarFileDstIsFile(t *testing.T) {
+	td, err := ioutil.TempDir("", "untartest")
+	if err != nil {
+		t.Fatalf("error creating temp dir: %v", err)
+	}
+	defer os.RemoveAll(td)
+
+	if _, err := makeFs(td, singleFile); err != nil {
+		t.Fatalf("error creating temp fs: %v", err)
+	}
+	tp := singleFile.files[0].name
+
+	tf, err := makeTar(td, tp)
+	if err != nil {
+		t.Fatalf("error creating taring src: %v", err)
+	}
+	defer tf.Close()
+	defer os.RemoveAll(tf.Name())
+
+	tr, err := ioutil.TempDir("", "tarres")
+	if err != nil {
+		t.Fatalf("error creating temp dir: %v", err)
+	}
+	defer os.RemoveAll(tr)
+
+	// create the remote file
+	dst := filepath.Join(tr, tp)
+	f, err := os.Create(dst)
+	if err != nil {
+		t.Fatalf("error creating existing file: %v", err)
+	}
+
+	if _, err := f.Write([]byte("foo")); err != nil {
+		t.Fatalf("error writing existing file: %v", err)
+	}
+	f.Close()
+
+	if err := Untar(tf, dst); err != nil {
+		t.Errorf("error untaring file: %v", err)
+		return
+	}
+
+	if _, err := os.Stat(dst); err != nil {
+		t.Errorf("error untaring file: %v", err)
+		return
+	}
+
+	if err := fileCompare(dst); err != nil {
+		t.Error(err)
+	}
+}
+
+// TestUntarFileDstNotExist /foo/bar/file.txt /remote/non_existing_path (create file)
+func TestUntarFileDstNotExist(t *testing.T) {
+	td, err := ioutil.TempDir("", "untartest")
+	if err != nil {
+		t.Fatalf("error creating temp dir: %v", err)
+	}
+	defer os.RemoveAll(td)
+
+	if _, err := makeFs(td, singleFile); err != nil {
+		t.Fatalf("error creating temp fs: %v", err)
+	}
+	tp := singleFile.files[0].name
+
+	tf, err := makeTar(td, tp)
+	if err != nil {
+		t.Fatalf("error creating taring src: %v", err)
+	}
+	defer tf.Close()
+	defer os.RemoveAll(tf.Name())
+
+	tr, err := ioutil.TempDir("", "tarres")
+	if err != nil {
+		t.Fatalf("error creating temp dir: %v", err)
+	}
+	defer os.RemoveAll(tr)
+
+	dst := filepath.Join(tr, "new_file.txt")
+	if err := Untar(tf, dst); err != nil {
+		t.Errorf("error untaring file: %v", err)
+		return
+	}
+
+	if _, err := os.Stat(dst); err != nil {
+		t.Errorf("error untaring file: %v", err)
+		return
+	}
 }
 
 func TestUntar(t *testing.T) {
@@ -194,7 +529,7 @@ func TestUntar(t *testing.T) {
 		tp := tc.path
 		if tc.path == "" {
 			// This is a single file test case
-			tp = tc.files[0]
+			tp = tc.files[0].name
 		}
 
 		tf, err := ioutil.TempFile("", "src.tar")
@@ -208,7 +543,7 @@ func TestUntar(t *testing.T) {
 
 		cmd := exec.Command("tar", args...)
 		if out, err := cmd.CombinedOutput(); err != nil {
-			t.Errorf("error taring cmd %v: %s, %v", args, out, err)
+			t.Fatalf("error taring cmd %v: %s, %v", args, out, err)
 		}
 
 		tr, err := ioutil.TempDir("", "tarres")
@@ -219,6 +554,7 @@ func TestUntar(t *testing.T) {
 
 		if err := Untar(tf, tr); err != nil {
 			t.Errorf("error untaring file: %v", err)
+			return
 		}
 
 		// Walk the original dir and compare against extracted
@@ -232,13 +568,16 @@ func TestUntar(t *testing.T) {
 
 		if err != nil {
 			t.Errorf("got error untaring: %v", err)
+			return
 		}
 		if len(seen) != len(all) {
 			t.Errorf("wrong number of files. Got %v expected %v", seen, all)
+			return
 		}
 		for _, f := range all {
 			if !seen[f] {
 				t.Errorf("file %s not in tared files", f)
+				return
 			}
 		}
 	}
