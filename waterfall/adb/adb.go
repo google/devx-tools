53845256f8577868a44feb8c5f85e6e8dfc00c0b
Mauricio Galindo <mauriciogg@google.com>
2018-08-10 19:47:48 -0700

    Added bootstrap to host forwarder daemon Design doc: go/h2o-mobile-install
    Change-Id: I4e5279044fe2fb4c0ab69ad15be65540d8e1fdca

---
+++ waterfall/adb/adb.go
@@ -0,0 +1,121 @@
+// Package adb implements a thin adb client to bootstrap h2o.
+package adb
+
+import (
+	"fmt"
+	"os/exec"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+const (
+	socketDirProp = "qemu.host.socket.dir"
+	abiListProp   = "ro.product.cpu.abilist"
+	abstractPort  = "localabstract:"
+)
+
+type Device struct {
+	AdbPath       string
+	DeviceName    string
+	AdbServerPort string
+	args          []string
+	argsOnce      sync.Once
+}
+
+func (a *Device) deviceArgs() []string {
+	a.argsOnce.Do(func() {
+		args := []string{"-s", a.DeviceName}
+		if a.AdbServerPort != "" {
+			args = append(args, "-P", a.AdbServerPort)
+		}
+		a.args = args
+	})
+	return a.args
+}
+
+func (a *Device) Shell(args []string) (string, error) {
+	fullArgs := append(a.deviceArgs(), "shell", strings.Join(args, " ")+"; echo ret=$?")
+	cmd := exec.Command(a.AdbPath, fullArgs...)
+	out, err := cmd.CombinedOutput()
+	o := string(out)
+
+	if err != nil {
+		return o, err
+	}
+
+	// Parse the exit status
+	r := o[strings.LastIndex(o, "ret="):]
+	o = strings.TrimSpace(o[:len(o)-len(r)])
+	if ret, err := strconv.Atoi(strings.TrimSpace(r[4:])); err != nil {
+		return "", fmt.Errorf("error parsing return code %v", r)
+	} else if ret != 0 {
+		return "", fmt.Errorf("non-zero return code %v = %s", args, o)
+	}
+	return o, nil
+}
+
+func (a *Device) Connect() error {
+	// Ignore error, verify the device is connected.
+	exec.Command(a.AdbPath, append(a.deviceArgs(), "connect", a.DeviceName)...).CombinedOutput()
+
+	// adb connect is actually verified by the follwowing code
+	ob, err := exec.Command(a.AdbPath, append(a.deviceArgs(), "devices")...).CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("got error running adb devices: %s %v", string(ob), err)
+	}
+
+	// adb devices sample output:
+	// List of devices attached
+	// 127.0.0.1:40001	device
+
+	out := string(ob)
+	ls := strings.Split(out, "\n")
+	for _, l := range ls {
+		if strings.Contains(l, a.DeviceName) && strings.Contains(l, "device") {
+			return nil
+		}
+	}
+	return fmt.Errorf("%s not connected. devices output:\n %s", a.DeviceName, out)
+}
+
+func (a *Device) Push(src, dst string) error {
+	cmd := exec.Command(a.AdbPath, append(a.deviceArgs(), "push", src, dst)...)
+	if out, err := cmd.CombinedOutput(); err != nil {
+		return fmt.Errorf("error pushing %s - %s", src, string(out))
+	}
+	return nil
+}
+
+func (a *Device) QemuPipeDir() (string, error) {
+	p, err := a.Shell([]string{"getprop", socketDirProp})
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(p), nil
+}
+
+func (a *Device) AbiList() (string, error) {
+	p, err := a.Shell([]string{"getprop", abiListProp})
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(p), nil
+}
+
+func (a *Device) ForwardAbstract(local, remote string) error {
+	cmd := exec.Command(a.AdbPath, append(a.deviceArgs(), "forward", abstractPort+local, abstractPort+remote)...)
+	if out, err := cmd.CombinedOutput(); err != nil {
+		return fmt.Errorf("error forwarding %s -> %s %s", local, remote, string(out))
+	}
+	return nil
+}
+
+func (a *Device) StartCmd(script string) error {
+	cmd := exec.Command(a.AdbPath, append(a.deviceArgs(), "shell", script)...)
+	if err := cmd.Start(); err != nil {
+		return fmt.Errorf("error running script %s %v", err)
+	}
+	cmd.Process.Release()
+	return nil
+}
---
+++ waterfall/forward/bootstrap.go
@@ -0,0 +1,221 @@
+package forward
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/waterfall/adb"
+	"github.com/waterfall/client"
+	"github.com/waterfall/net/qemu"
+	"google.golang.org/grpc"
+
+	waterfall_grpc "github.com/waterfall/proto/waterfall_go_grpc"
+)
+
+const (
+	helloMsg            = "hello"
+	waterfallBinPath    = "/data/local/tmp/waterfall"
+	waterfallScriptPath = "/data/local/tmp/waterfall.sh"
+
+	waterfallSh = `#!/system/bin/sh
+# start waterfall through adb
+# but make sure it does not die if adb dies
+waterfall_bin=%s
+addr=%s
+cd /data/local/tmp
+# prefer root if possible
+su -c nohup "${waterfall_bin}" --addr "${addr}" || nohup "${waterfall_bin}" --addr "${addr}"`
+)
+
+// BootstrapResult has information about a succesful bootstrap session.
+type BootstrapResult struct {
+	StartedServer    bool
+	StartedForwarder bool
+}
+
+func retry(fn func() error, attempts int, delay time.Duration) error {
+	var err error
+	for i := 0; i < attempts; i++ {
+		err = fn()
+		if err == nil {
+			return nil
+		}
+		time.Sleep(delay)
+	}
+	return err
+}
+
+func pingServer(addr string) error {
+	cc, err := grpc.Dial(addr, grpc.WithInsecure())
+	if err != nil {
+		return err
+	}
+
+	wc := waterfall_grpc.NewWaterfallClient(cc)
+	resp, err := client.Echo(context.Background(), wc, []byte(helloMsg))
+	if err != nil {
+		return err
+	}
+
+	if string(resp) != helloMsg {
+		return fmt.Errorf("unexpected response message: %s", string(resp))
+	}
+	return nil
+}
+
+func connectToPipe(socketDir, socketName string) error {
+	qb, err := qemu.MakeConnBuilder(socketDir, socketName)
+	if err != nil {
+		return err
+	}
+	defer qb.Close()
+
+	connCh := make(chan net.Conn, 1)
+	errCh := make(chan error, 1)
+
+	// If the server is not running this will block waiting for a connection.
+	go func() {
+		c, err := qb.Next()
+		if err != nil {
+			errCh <- err
+			return
+		}
+		c.Close()
+		connCh <- c
+	}()
+
+	select {
+	case <-connCh:
+		return nil
+	case err := <-errCh:
+		return err
+	case <-time.After(time.Millisecond * 300):
+		return fmt.Errorf("timed out")
+	}
+
+}
+
+func startWaterfall(adbConn *adb.Device, wtf, addr string) error {
+	if err := adbConn.Push(wtf, waterfallBinPath); err != nil {
+		return err
+	}
+
+	sc := fmt.Sprintf(waterfallSh, waterfallBinPath, addr)
+	dir, err := ioutil.TempDir("", "waterfall")
+	defer os.RemoveAll(dir)
+
+	name := filepath.Join(dir, "waterfall.sh")
+	if err := ioutil.WriteFile(name, []byte(sc), 0766); err != nil {
+		return err
+	}
+
+	if err := adbConn.Push(name, waterfallScriptPath); err != nil {
+		return err
+	}
+
+	err = adbConn.StartCmd(waterfallScriptPath)
+	return err
+}
+
+func startForwarder(forwarderBin, lisAddr, connAddr string) error {
+	// fork a new process with the right params.
+	cmd := exec.Command(forwarderBin, "-connect_addr", connAddr, "-listen_addr", lisAddr)
+
+	if err := cmd.Start(); err != nil {
+		return err
+	}
+	cmd.Process.Release()
+	return nil
+}
+
+func waterfallPath(adbConn *adb.Device, paths []string) (string, error) {
+	abis, err := adbConn.AbiList()
+	if err != nil {
+		return "", err
+	}
+
+	archPaths := make(map[string]string)
+	for _, p := range paths {
+		pts := strings.Split(p, ":")
+		if len(pts) != 2 {
+			return "", fmt.Errorf("can't parse server path %s", p)
+		}
+		archPaths[pts[0]] = pts[1]
+	}
+
+	for _, abi := range strings.Split(abis, ",") {
+		if p, ok := archPaths[abi]; ok {
+			return p, nil
+		}
+	}
+	return "", fmt.Errorf("no valid server for architectures %s got %v", abis, paths)
+}
+
+func Bootstrap(adbConn *adb.Device, waterfallBin []string, forwarderBin, socketName string) (*BootstrapResult, error) {
+	// The default bootstrap address.
+	unixName := fmt.Sprintf("h2o_%s", adbConn.DeviceName)
+	lisAddr := fmt.Sprintf("unix:@%s", unixName)
+
+	if err := pingServer(lisAddr); err == nil {
+		// There is responsive forwarder server already running
+		return &BootstrapResult{}, nil
+	}
+
+	socketDir, err := adbConn.QemuPipeDir()
+	if err != nil {
+		return nil, fmt.Errorf("error getting pipe dir: %v", err)
+	}
+
+	svr, err := waterfallPath(adbConn, waterfallBin)
+	if err != nil {
+		return nil, err
+	}
+
+	ss := false
+	sf := false
+	if socketDir != "" {
+		// has qemu pipe support
+		socketDir = filepath.Dir(socketDir)
+		connAddr := fmt.Sprintf("qemu:%s:%s", socketDir, socketName)
+		svrAddr := fmt.Sprintf("qemu:%s", socketName)
+
+		// Check if the server is listening on the qemu pipe first
+		if connectToPipe(socketDir, socketName) != nil {
+			if err := startWaterfall(adbConn, svr, svrAddr); err != nil {
+				return nil, err
+			}
+			ss = true
+		}
+		if err := startForwarder(forwarderBin, lisAddr, connAddr); err != nil {
+			return nil, err
+		}
+		sf = true
+	} else {
+		// will tunnel through adb
+		if err := adbConn.ForwardAbstract(unixName, unixName); err != nil {
+			return nil, fmt.Errorf("error forwarding through adb: %v", err)
+		}
+
+		if err := pingServer(lisAddr); err != nil {
+			if err := startWaterfall(adbConn, svr, lisAddr); err != nil {
+				return nil, err
+			}
+			ss = true
+			sf = false
+		}
+
+	}
+
+	// Verify everything was set up correctly before confirming to client
+	if err := retry(func() error { return pingServer(lisAddr) }, 3, time.Millisecond*300); err != nil {
+		return nil, err
+	}
+	return &BootstrapResult{StartedServer: ss, StartedForwarder: sf}, nil
+}
